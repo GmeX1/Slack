88a94570ed4ad4eaf8556be0fb22221be7d6280f
GmeX1 <gmex51@yandex.ru>
2024-01-10 23:42:48 +0300

    подгрузка миксера

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from player_class import Bullet, Enemy, Player
 from scripts import database_create, generate_tiles, show_fps, time_convert
 from small_logic_classes import Camera, Level
 
+pygame.mixer.pre_init(44100, -16, 1, 512)
 pygame.init()
 pygame.display.set_caption('Slack')
 info = pygame.display.Info()
@@ -41,6 +42,23 @@ def load_image(name, colorkey=None):
 
 
 def start_game():
+    # music_volume = db.cursor().execute('SELECT value FROM settings WHERE name="music_volume"').fetchone()[0] / 100
+    # pygame.mixer.music.load('data\\music\\level_1\\main_layer.wav')
+    # layer_1 = pygame.mixer.Channel(0)  # TODO: Завернуть в класс
+    # layer_1_sound = pygame.mixer.Sound('data\\music\\level_1\\layer_1.wav')
+    # layer_1.set_volume(0)
+    # layer_2 = pygame.mixer.Channel(1)
+    # layer_2_sound = pygame.mixer.Sound('data\\music\\level_1\\layer_2.wav')
+    # layer_2.set_volume(0)
+    # layer_3 = pygame.mixer.Channel(2)
+    # layer_3_sound = pygame.mixer.Sound('data\\music\\level_1\\layer_3.wav')
+    # layer_3.set_volume(0)
+    #
+    # pygame.mixer_music.play(-1)
+    # layer_1.play(layer_1_sound, -1)
+    # layer_2.play(layer_2_sound, -1)
+    # layer_3.play(layer_3_sound, -1)
+
     bullet_icon = load_image('bullet\\bullet.png')
 
     all_sprites = pygame.sprite.Group()
